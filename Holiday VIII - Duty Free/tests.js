a59fd6fb8bb1eeab98425ade6601ed60898ebe00
lukmil <lmilasevicius@gmail.com>
2017-01-26 12:53:37 +0200

    Holiday VIII - Duty Free ( papildyta su 0 reiskme)

--- Holiday VIII - Duty Free/tests.js
+++ Holiday VIII - Duty Free/tests.js
@@ -1,6 +1,6 @@
 function dutyFree(normPrice, discount, hol) {
     if (normPrice === null || discount === null || hol === null) throw new Error("Any Value given null");
-    if (normPrice  < 0 || discount  < 0 || hol < 0) throw new Error("Any Value given with minus");
+    if (normPrice  <= 0 || discount  <= 0 || hol <= 0) throw new Error("Any Value given with minus or 0");
     return Math.floor((hol / (normPrice / 100 * discount)));
 }
 
@@ -12,6 +12,10 @@ QUnit.test("Holiday VIII - Duty Free", function (assert) {
     assert.throws(function () {
         dutyFree(-1, -1, -1);
     }, Error);
+    assert.throws(function () {
+        dutyFree(0, 0, 0);
+    }, Error);
+
     assert.equal(dutyFree(12, 50, 1000), 166);
     assert.equal(dutyFree(17, 10, 500), 294);
     assert.equal(dutyFree(24, 35, 3000), 357);
